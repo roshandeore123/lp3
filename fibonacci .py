0513f4033b1eb1e435ae497087b8324f5eb4a7d0
Roshan Kashinath Deore <roshandeore702@gmail.com>
2025-11-12 20:41:24 +0530

    Create fibonacci .py

---
+++ fibonacci .py
@@ -0,0 +1,39 @@
+# Program to display the Fibonacci sequence up to n-th term(non recursion )
+nterms = int(input("Enter number of terms "))
+# first two terms
+n1, n2 = 0, 1
+count = 0
+# check if the number of terms is valid
+if nterms <= 0: 
+    print("Please enter a positive integer")
+# if there is only one term, return n1
+elif nterms == 1: 
+    print("Fibonacci sequence upto", nterms,":")
+    print(n1)
+# generate fibonacci sequence
+else: 
+    print("Fibonacci sequence:") 
+    while count < nterms:
+        print(n1) 
+        nth = n1 + n2
+       # update values
+        n1 = n2 
+        n2 = nth
+        count += 1
+
+
+(recursion)
+
+
+def fibonacci(n):
+    if(n <= 1):
+        return n
+    else:
+        return(fibonacci(n-1) + fibonacci(n-2))
+n = int(input("Enter number of terms:"))
+print("Fibonacci sequence:")
+for i in range(n):
+    print(fibonacci(i))
+
+
+
